20fa27d4128bb627914fb5bb519b1bfed0e57c5e
jansteinarsaetre <jan.s.saetre@icloud.com>
2026-03-08 21:20:15 +0100

    test

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/ads.txt')
-def ads():
-    return render_template("ads.txt")
+
+@app.route('/registrer', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+            username = request.form['username']
+    return render_template('registrer.html')
+@app.route('/login', methods={'GET', 'POST'})
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    return render_template('login.html')
+
 
 
 if __name__ == '__main__':
